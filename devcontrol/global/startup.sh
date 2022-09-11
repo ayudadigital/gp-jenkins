2249dab6aae4845b1dddfc1840fc314429d07ff2
Pedro Amador <pedroamador.rodriguez@gmail.com>
2022-09-11 17:24:02 +0200

    Upgrade: Use jenkins-dind v2.363.1

--- devcontrol/global/startup.sh
+++ devcontrol/global/startup.sh
@@ -2,5 +2,5 @@
 
 # @file devcontrol/global/startup.sh
 # @brief devcontrol startup script and functions
-echo "Generic Platform - Jenkins Service (c) 2020 Ayuda Digital"
+echo "Generic Platform - Jenkins Service (c) 2020-2022 Ayuda Digital"
 echo
